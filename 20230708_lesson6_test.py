f91ac96555d5f8b9c8224b99c3cd775eb25e0589
junjunkatetsai <136140316+junjunkatetsai@users.noreply.github.com>
2023-07-08 08:16:57 +0000

    modify

---
+++ 20230708_lesson6_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3.10.4
+
+import random
+
+def playgame():
+    min = 1
+    max = 100
+    count = 0
+    target = random.randint(min,max)
+    print(target)
+    print('----猜數字遊戲-----\n')
+
+    while True:
+        keyin = int(input(f'猜數字範圍{min}~{max}:'))
+        count += 1
+        if (keyin == target):
+            print(f'賓果! 猜對了,答案是:{target}')
+            print(f'您共猜了{count}次')
+            break
+        elif keyin > target:
+            print(f'再小一點')
+            max = keyin - 1
+        elif keyin < target:
+            print(f'再大一點')
+            min = keyin + 1
+        print(f'您共猜了{count}次')
+
+while True:
+    playgame():
+    play_again = input('再玩一次( Y / N )')
+    if not(play_again == y):
+        break
+
+print('遊戲結束')
