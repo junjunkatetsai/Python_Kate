8aabde55f70d421ca517b53969927c0565795e5f
junjunkatetsai <136140316+junjunkatetsai@users.noreply.github.com>
2023-08-12 07:57:32 +0000

    modify

--- taiwan_weather_230812/20230812_main.py
+++ taiwan_weather_230812/20230812_main.py
@@ -81,6 +81,11 @@ if not check_file_exist():
 
 file_path = get_fileName_path()
 dataFrame = pd.read_csv(file_path)
+dataFrame['啟始時間'] = pd.to_datetime(dataFrame['啟始時間'])
+dataFrame['結束時間'] = pd.to_datetime(dataFrame['結束時間'])
+dataFrame['啟始時間'] = dataFrame['啟始時間'].dt.strftime('%Y-%m-%d日-%H點')
+dataFrame['結束時間'] = dataFrame['結束時間'].dt.strftime('%Y-%m-%d日-%H點')
+
 #顯示標題
 st.title("台灣個縣市氣候:")
 st.subheader("攝氐")
